2ae6c06337ebb1d9411df791704f44c04d552928
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2023-03-01 11:47:25 +0700

    Update: Year and `CMakeLists.txt`

--- dotenv/dotenv.cpp
+++ dotenv/dotenv.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 harshfeudal
+ * Copyright 2023 harshfeudal
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
