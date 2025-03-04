7b8cbad32f9fc7f48babe7b52ee338033b7d68c5
harshfeudal <sdanghuutrung@gmail.com>
2025-03-04 12:55:42 +0700

    Fix: Minor update

--- run.bat
+++ run.bat
@@ -19,11 +19,10 @@ cmake -G "Visual Studio 17 2022" -A x64 ..
 cmake --build . --config Release -j8
 
 if %errorlevel% equ 0 (
-    echo %ESC%[1;32mBuild succeeded. Opening test_dotenv.exe ...%ESC%[0m
-    copy ..\.env .\Release
+    echo %ESC%[1;32mBuild succeeded. Opening Dotenv.exe ...%ESC%[0m
     cls
     cd Release
-    test_dotenv.exe
+    Dotenv.exe
 ) else (
     echo %ESC%[1;31mBuild failed!%ESC%[0m
 )
